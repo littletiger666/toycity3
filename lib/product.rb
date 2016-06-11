17fdf5aed3a5975c8347adbaa3fe2da027914ea5
Xiaohu Li <li.xiaohu6@gmail.com>
2016-06-11 16:20:34 -0700

    add find function and stock search function

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,46 @@
 class Product
+  attr_reader :title, :price, :stock
+
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  def self.find_by_title name
+    item = @@products.select {|product| product.title == name}
+    item[0]
+  end
+
+  def in_stock?
+    if self.stock > 0
+      return true
+    else
+      return false
+    end
+  end
+
+  def self.in_stock
+    @@products.select {|product| product.stock > 0}
+  end
+
+
+
+  private
+
+  def add_to_products
+    name = @@products.map {|product| product.title}
+    unless name.include? self.title
+      @@products << self
+    else
+      raise DuplicateProductError, "#{self.title} already exists"
+    end
   end
 end
