0d1007c452c986bf0c82c5e8e99c19e212f94bbe
Xiaohu Li <li.xiaohu6@gmail.com>
2016-06-11 16:52:32 -0700

    add class Customer

---
+++ lib/customer.rb
@@ -0,0 +1,30 @@
+class Customer
+  attr_reader :name
+
+  @@customers = []
+
+  def initialize options={}
+    @name = options[:name]
+    add_to_customers
+  end
+
+  def self.all
+    @@customers
+  end
+
+  def self.find_by_name name
+    person = @@customers.select {|customer| customer.name == name}
+    person[0]
+  end
+
+  private
+
+  def add_to_customers
+    c_name = @@customers.map {|person| person.name}
+    unless c_name.include? self.name
+      @@customers << self
+    else
+      raise DuplicateProductError, "#{self.name} already exists"
+    end
+  end
+end
